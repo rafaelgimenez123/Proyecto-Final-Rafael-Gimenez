7485f0266bc5e222206d15e5ce576f3a260db356
rafaelgimenez123 <107961108+rafaelgimenez123@users.noreply.github.com>
2023-09-13 16:25:41 -0300

    nuevo

--- proyecto-final-reactjs/src/ShoppingCartContext/ShoppingCartContext.jsx
+++ proyecto-final-reactjs/src/ShoppingCartContext/ShoppingCartContext.jsx
@@ -3,6 +3,25 @@ import React, { createContext, useState } from "react";
 export const CartContext = createContext(null);
 
 export const ShoppingCartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]); // Corrected the typo here
-  return <CartContext.Provider value={[cart, setCart]}>{children}</CartContext.Provider>;
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    const isItemFound = cart.find((cartItem) => cartItem.id === item.id);
+
+    if (isItemFound) {
+      const updatedCart = cart.map((cartItem) =>
+        cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+      );
+
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, { ...item, quantity: 1 }]);
+    }
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
