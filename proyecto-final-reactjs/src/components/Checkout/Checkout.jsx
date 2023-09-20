d54149ccb3934a7aed4fbb6f8b7add150727970c
rafaelgimenez123 <107961108+rafaelgimenez123@users.noreply.github.com>
2023-09-20 20:26:06 -0300

    checkoutbeta

--- proyecto-final-reactjs/src/components/Checkout/Checkout.jsx
+++ proyecto-final-reactjs/src/components/Checkout/Checkout.jsx
@@ -1,25 +1,24 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import CartContext from "../../context/CartContext";
 import { serverTimestamp } from "firebase/firestore";
 import { getCartTotal, mapCartToOrderItems } from "../../Utils";
 import { createOrder } from "../../services";
+import CartProvider from "../../context/CartProvider";
 
 const Checkout = () => {
   const [orderId, setOrderId] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const { cart, clear } = useContext(CartContext);
 
-  const total = getCartTotal(cart);
-
   const handleCheckout = () => {
     const order = {
       buyer: {
-        name: "John",
-        phone: "123456789",
-        email: "john@gmail.com",
+        name: document.getElementById("name").value, 
+        phone: document.getElementById("phone").value, 
+        email: document.getElementById("email").value, // 
       },
       items: mapCartToOrderItems(cart),
-      total,
+      total: getCartTotal(cart),
       date: serverTimestamp(),
     };
 
@@ -43,7 +42,23 @@ const Checkout = () => {
         <>
           <div className="mb-4">
             <h4>Formulario de contacto</h4>
-            {/* TODO: Formulario */}
+            <form>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">Nombre:</label>
+                <input type="text" id="name" name="name" className="form-control" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="phone" className="form-label">Teléfono:</label>
+                <input type="text" id="phone" name="phone" className="form-control" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Correo electrónico:</label>
+                <input type="email" id="email" name="email" className="form-control" />
+              </div>
+              <button type="button" className="btn btn-primary" onClick={handleCheckout}>
+                Finalizar compra
+              </button>
+            </form>
           </div>
 
           <div className="mb-4">
@@ -51,7 +66,7 @@ const Checkout = () => {
             <ul className="list-group">
               {cart.map((item) => (
                 <li key={item.id} className="list-group-item">
-                  <p>Nombre del producto:{item.title}</p>
+                  <p>Nombre del producto: <b>{item.name}</b></p>
                   <p>Cantidad: {item.quantity}</p>
                   <p>Precio por unidad: ${item.price}</p>
                   <p>Subtotal: ${item.price * item.quantity}</p>
@@ -60,11 +75,7 @@ const Checkout = () => {
             </ul>
           </div>
 
-          <p>Total de la compra: ${total}</p>
-
-          <button className="btn btn-primary" onClick={handleCheckout}>
-            Finalizar compra
-          </button>
+          <p>Total de la compra: ${getCartTotal(cart)}</p>
 
           {isLoading && <p>Procesando compra...</p>}
         </>
